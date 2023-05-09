4accdc2efc4a7d05c696215dfe779585c5820d0d
dachir <dodziamouzou@gmail.com>
2023-05-09 11:36:08 +0000

    090520231236

--- ls_treso/ls_treso/doctype/axe_analytique/axe_analytique.py
+++ ls_treso/ls_treso/doctype/axe_analytique/axe_analytique.py
@@ -1,8 +1,8 @@
-# Copyright (c) 2022, Kossivi Amouzou and contributors
+# Copyright (c) 2023, Kossivi Amouzou and contributors
 # For license information, please see license.txt
 
 # import frappe
 from frappe.model.document import Document
 
-class CompteAnalytique(Document):
+class AxeAnalytique(Document):
 	pass
--- ls_treso/ls_treso/doctype/compte_analytique/compte_analytique.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, Kossivi Amouzou and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Compte Analytique', {
-	// refresh: function(frm) {
-
-	// }
-});
